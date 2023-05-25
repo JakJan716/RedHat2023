094bf8df9ab7c8b356a9f4fe90f302545f4f5623
JakJan716 <jakoubek.janek@gmail.com>
2023-05-25 14:57:08 +0200

    Pridani test_sample

--- python/test_sample.py
+++ python/test_sample.py
@@ -1,13 +1,12 @@
 import pytest
 
+def func(test_in):
+    return test_in + 1
+
 @pytest.mark.parametrize(
 "test_in, expected", 
 [(3, 4), (5, 6), (6, 7)],
 )
-
-def func(test_in):
-    return test_in + 1
-
 
 def test_answer(test_in, expected):
     assert func(test_in) == expected
