a78b612150c1919f2f9118f8c6d30429c339d503
JakJan716 <jakoubek.janek@gmail.com>
2023-05-25 14:49:17 +0200

    Pridani testu pro obsah

--- python/test_python.py
+++ python/test_python.py
@@ -4,6 +4,10 @@ def obvodctverce(a):
     obvod = 4*a
     print(f"Obvod ctverce je: {obvod}")
 
+def obsahctverce(a):
+    obsah = a**2
+    print(f"Obsah ctverce je: {obsah}")
+
 @pytest.mark.parametrize(
 "a, expected",
 [(5, 20), (6, 24)]
@@ -12,3 +16,10 @@ def obvodctverce(a):
 def test_obvod_ctverce(a, expected):
     obvodctverce(a) == expected
 
+@pytest.mark.parametrize(
+"a, expected",
+[(5, 25), (6, 36)]
+)
+
+def test_obsah_ctverce(a, expected):
+    obsahctverce(a) == expected
